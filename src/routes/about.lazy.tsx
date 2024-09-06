e5cf31c0d531be4da6e123e73cb797c6f14055f1
AdelaChmel <adela.chmelikova97@gmail.com>
2024-09-06 17:36:33 +0200

    Add TanStackRouter

---
+++ src/routes/about.lazy.tsx
@@ -0,0 +1,13 @@
+import { createLazyFileRoute } from '@tanstack/react-router'
+
+export const Route = createLazyFileRoute('/about')({
+  component: About,
+})
+
+function About() {
+  return (
+    <div> 
+      Hello from About!
+    </div>
+  )
+}
