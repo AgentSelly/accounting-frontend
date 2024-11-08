1f415b95cc275e034b7dccf42fb6f505f171928e
AdelaChmel <adela.chmelikova97@gmail.com>
2024-11-08 15:32:59 +0100

    Change logo and title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
      return (
           <header className="border-4 border-slate-600 rounded-md flex items-center">
-               <img src="src\assets\react.svg" className="p-2"></img>
-               <h1 className="text-3xl">My company</h1>
+               <img src="src\assets\logo.svg" className="p-2"></img>
+               <h1 className="text-3xl">Accounting AgentSelly</h1>
           </header>
      )
 }
