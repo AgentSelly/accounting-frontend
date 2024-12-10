14db2d101b5e5945b674ec96dba780c17ec2ad3b
AdelaChmel <adela.chmelikova97@gmail.com>
2024-12-10 18:22:13 +0100

    Style login page

--- src/routes/login.lazy.tsx
+++ src/routes/login.lazy.tsx
@@ -6,18 +6,19 @@ export const Route = createLazyFileRoute('/login')({
 
 function Login() {
   return (
-    <div className="p-2">
-      <div className="flex flex-col justify-center">
-        <img src="src\assets\avatar.svg" className='w-24 h-24' />
-        <form>
-          <label>Enter your name:
-            <input type="text" />
-          </label>
-          <label>Enter your email:
-            <input type="text" />
-          </label>
+      <div className="p-4 px-10 flex flex-col justify-center gap-2 w-fit border-2 border-secondary-500 rounded-md">
+        <h1 className="text-3xl font-bold">Sign in</h1>
+        <form >
+          <p className="mb-8">Enter your email to receive a one-time passcode.</p>
+          <input type="text" placeholder="amelie@untitledui.com" className="input w-full border-2 border-secondary-500" />
+          <div className="mt-2 form-control">
+            <label className="label justify-start cursor-pointer">
+              <input type="checkbox" className="toggle" defaultChecked />
+              <p className="label-text font-bold pl-2">Remember me for 30 days</p>
+            </label>
+          </div>
+          <button className="btn btn-block mt-6 border-secondary-500 bg-secondary-500 hover:bg-secondary-600 text-white">Send 4-digit code</button>
         </form>
       </div>
-    </div>
   )
 }
